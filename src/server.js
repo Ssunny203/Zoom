eea147cbb1bc37e8ef562b8cf531c5cb82f6f50e
CHOI JISEON <121207991+Ssunny203@users.noreply.github.com>
2023-09-05 14:43:02 +0900

    Finishing the introduction session

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views"); // __dirname을 호출하면 같은 디렉토리 경로가 반환됨
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => console.log('Listening on http://localhost:3000');
+app.listen(3000, handleListen);
