7362ad6e9452c2a0a15f1f7996601917e6997540
CHOI JISEON <121207991+Ssunny203@users.noreply.github.com>
2023-09-06 17:37:38 +0900

    09/06 testing websocket

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,26 @@
 const frontSocket = new WebSocket(`ws://${window.location.host}`); //socket = 서버로의 연결
+const messageList = document.querySelector("ul");
+const messageForm = document.querySelector("form");
+
+
+function handleOpen() {
+    console.log("Connected to Server ✅");
+}
+
+function handleMessage(message) {
+    console.log("New message :", message.data, " from the server");
+}
+
+function handleClose() {
+    console.log("Disconnected from Server ⛔");
+}
+
+frontSocket.addEventListener("open", handleOpen);
+
+frontSocket.addEventListener("message", handleMessage);
+
+frontSocket.addEventListener("close", handleClose);
+
+setTimeout(() => {
+    frontSocket.send("Hi! I'm browser.")
+}, 10000);
--- src/server.js
+++ src/server.js
@@ -15,9 +15,19 @@ const handleListen = () => console.log("Listening on http://localhost:3000");
 const server = http.createServer(app);
 const wss = new WebSocketServer.Server({ server }); //http 서버 위에 webSocket 서버를 만듦
 
+function onSocketMessage(message) {
+    console.log(message.toString("utf8"));
+}
+
+function onSocketClose() {
+    console.log("Disconnected from the Browser ⛔");
+}
+
 wss.on("connection", (backSocket) => { //socket = 연결된 브라우저
-    console.log(backSocket);
+    console.log("Connected to Browser ✅");
     backSocket.send("hello");
+    backSocket.on("message", onSocketMessage);
+    backSocket.on("close", onSocketClose);
 });
 
 server.listen(3000, handleListen);
