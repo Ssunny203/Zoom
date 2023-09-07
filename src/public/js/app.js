f26903635c086f24eac6f1b4db68d87e04090c97
CHOI JISEON <121207991+Ssunny203@users.noreply.github.com>
2023-09-07 18:01:56 +0900

    09/07 making websocket chat

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,33 +1,43 @@
 const frontSocket = new WebSocket(`ws://${window.location.host}`); //socket = 서버로의 연결
 const messageList = document.querySelector("ul");
-const messageForm = document.querySelector("form");
-
+const nickForm = document.querySelector("#nickname");
+const messageForm = document.querySelector("#message");
 
 function handleOpen() {
     console.log("Connected to Server ✅");
 }
 
 function handleMessage(message) {
-    console.log("New message :", message.data, "from the server");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 }
 
 function handleClose() {
     console.log("Disconnected from Server ⛔");
 }
 
-frontSocket.addEventListener("open", handleOpen);
-
-frontSocket.addEventListener("message", handleMessage);
-
-frontSocket.addEventListener("close", handleClose);
-
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg); //JSON.stringify() : JavaScript object를 string으로 바꿔줌
+}
 
 function handleSubmit(event) {
     event.preventDefault();
     const input = messageForm.querySelector("input");
-    frontSocket.send(input.value);
+    frontSocket.send(makeMessage("new_message", input.value));
     input.value = "";
 }
 
-messageForm.addEventListener("submit", handleSubmit);
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickForm.querySelector("input");
+    frontSocket.send(makeMessage("nickname", input.value));
+}
 
+frontSocket.addEventListener("open", handleOpen);
+frontSocket.addEventListener("message", handleMessage);
+frontSocket.addEventListener("close", handleClose);
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import http from "http";
 import WebSocketServer from "ws";
 
-const app = express();
+const app = express(); //서버 셋팅
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views"); // __dirname을 호출하면 같은 디렉토리 경로가 반환됨
@@ -23,11 +23,27 @@ const sockets = [];
 
 wss.on("connection", (backSocket) => { //socket = 연결된 브라우저
     sockets.push(backSocket);
+    backSocket["nickname"] = "anon";
     console.log("Connected to Browser ✅");
-    backSocket.send("hello");
-    backSocket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message.toString()));
-        //console.log(message.toString());
+    backSocket.send("hello!");
+    backSocket.on("message", (msg) => {
+        const message = JSON.parse(msg); // JSON.parse()는 string을 JavaScript object로 바꿔줌
+        console.log(message.toString());
+        
+        // if(message.type === "new_message"){
+        //     sockets.forEach((aSocket) => aSocket.send((message.payload).toString()));
+        // }
+        // else if(message.type === "nickname"){
+        //     console.log((message.payload).toString());
+        // }
+        
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket) => aSocket.send(`${backSocket.nickname}: ${(message.payload).toString()} `));
+            case "nickname":
+                backSocket["nickname"] = (message.payload).toString();
+        }
+
     });
     backSocket.on("close", onSocketClose);
 });
